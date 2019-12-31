e322ada8af5258cde454cab2af7f838a52c6de36
Scott Thiessen <scott@movingobjects.io>
2019-12-30 21:06:25 -0800

    Add initial dump of files

---
+++ src/components/App.react.js
@@ -0,0 +1,41 @@
+
+// Imports
+
+import * as React from 'react';
+
+
+// Constants
+
+
+// Class
+
+export default class App extends React.Component {
+
+  // Constructor
+
+  constructor() {
+
+    super();
+
+    this.state = { };
+
+  }
+
+
+  // Event handlers
+
+
+  // Methods
+
+
+  // React
+
+  render() {
+
+    return (
+      <p>PARTYYY PPL</p>
+    );
+
+  }
+
+}
